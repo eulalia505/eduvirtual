2e03f413ec4373f162f273aced03cfd170abb4fd
eulalia505 <100749548+eulalia505@users.noreply.github.com>
2022-03-05 16:34:42 -0500

    Update MoodleClient.py

--- MoodleClient.py
+++ MoodleClient.py
@@ -11,7 +11,7 @@ class MoodleClient(object):
         self.username = user
         self.password = passw
         self.session = requests.Session()
-        self.path = 'https://posgrado.unica.cu/login/index.php'
+        self.path = 'https://posgrado.unica.cu/'
         self.userdata = ''
 
     def getsession(self):
